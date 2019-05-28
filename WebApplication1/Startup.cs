0a5713b87309a32f671a684df4cf2216d88dc5b2
sebastianManolache <sebastian.manolache@assist.ro>
2019-05-28 09:39:37 +0300

    NetCore-develop-Setup

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -15,7 +15,6 @@ namespace WebApplication1
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_2 );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,13 +24,11 @@ namespace WebApplication1
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseHsts();
-            }
 
-            app.UseHttpsRedirection();
-            app.UseMvc();
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
         }
     }
 }
